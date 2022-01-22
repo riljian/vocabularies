81c10aecbba9863aa8987732a8a01531effa0750
Jian-Min Lin <riljian.tw@gmail.com>
2022-01-22 17:48:30 +0800

    chore: integrate with MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,33 @@
-import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import type { AppProps as NextAppProps } from 'next/app'
+import Head from 'next/head'
+import theme from '../configs/theme'
+import { createEmotionCache } from '../helpers/theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface EmotionAppProps extends NextAppProps {
+  emotionCache?: EmotionCache
 }
 
-export default MyApp
+function App({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: EmotionAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>{pageProps.title}</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
+
+export default App
---
+++ pages/_document.tsx
@@ -0,0 +1,61 @@
+import createEmotionServer from '@emotion/server/create-instance'
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
+import theme from '../configs/theme'
+import { createEmotionCache } from '../helpers/theme'
+
+// noinspection HtmlRequiredTitleElement
+export default class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          {/* Inject MUI styles first to match with the `prepend: true` configuration. */}
+          {(this.props as any).emotionStyleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+Document.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage
+  // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhancedApp(props) {
+          // See EmotionAppProps in _app.tsx
+          // @ts-ignore
+          return <App emotionCache={cache} {...props} />
+        },
+    })
+
+  const initialProps = await NextDocument.getInitialProps(ctx)
+  // This is important. It prevents emotion to render invalid HTML.
+  // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html)
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ))
+
+  return { ...initialProps, ...emotionStyleTags }
+}
