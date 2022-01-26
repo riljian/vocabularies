c7d6fa604c0441da4e24340c1502226c51e905ec
Jian-Min Lin <riljian.tw@gmail.com>
2022-01-26 08:55:10 +0800

    feat: adopt iconify

--- configs/theme.ts
+++ configs/theme.ts
@@ -1,5 +1,15 @@
 import { createTheme } from '@mui/material'
 
-const theme = createTheme()
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        svg.iconify {
+          font-size: 1.5em;
+        }
+      `,
+    },
+  },
+})
 
 export default theme
